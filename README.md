9e62d7f6aea8d8a3fea28f5944577b907343258c
danjmercurio <dan.j.mercurio@gmail.com>
2015-01-16 23:43:47 -0800

    for real this time

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ Uses sixohsix's twitter API library to harvest follower IDs from twitter account
 Example:
 
 ```python
->>> from twitteridgrabber import auth, get_chunkList
->>> from credentials import parameters
->>> l = get_chunkList(auth(),'djm000',-1,[])
+>>> from twitteridgrabber import get_chunkList
+>>> l = get_chunkList('djm000')
 >>> l
 [1330715556, 826311415, 425486512, 177488625, 69991609, 292139488, 412020405, 36
 6403598, 15961385, 300639080, 101244995]
->>>
 ```
+
+## Function Reference
+
+### get_chunkList(screen name, api object, initial cursor, reference to list)
+
+Recursive function that returns a list of twitter user ids as integers. Takes a string containing twitter username. Other optional arguments:
+
+
+api object: Use this argument if you are dealing with multiple parallel connections to twitter.
+
+cursor: Twitter returns paginated sets of results at 5000 ids per page. Typically you want to start from the beginning, but if for some reason you want to omit the earlier pages, set this argument to the integer value of the cursor id where you would like to begin.
+
+reference to list: If you already have a list instantiated and want to append twitter ids to it, pass it in here. By default this is just an empty list, or [].
--- twitteridgrabber.py
+++ twitteridgrabber.py
@@ -3,7 +3,11 @@
 from twitter import *
 from credentials import parameters
 
-def get_chunkCSV(api_object,screen_name,cursor):
+bearer_token = oauth2_dance(parameters['consumer_key'],parameters['consumer_secret'])
+api = Twitter(auth=OAuth2(bearer_token=bearer_token), retry=True)
+
+
+def get_chunkCSV(screen_name,api_object=api,cursor=-1):
 	followers = api_object.followers.ids(screen_name=screen_name, cursor=cursor)
 	next_cursor = followers[u'next_cursor']
 	chunk = followers[u'ids']
@@ -15,37 +19,30 @@ def get_chunkCSV(api_object,screen_name,cursor):
 		if (next_cursor == 0):
 			return
 		else:
-			return get_chunkCSV(api_object, screen_name, next_cursor)
+			return get_chunkCSV(screen_name, api_object=api_object, cursor=next_cursor)
 
-def get_chunkList(api_object,screen_name,cursor,op_list):
+def get_chunkList(screen_name, api_object=api,cursor=-1,op_list=[]):
 	followers = api_object.followers.ids(screen_name=screen_name, cursor=cursor)
 	next_cursor = followers[u'next_cursor']
 	chunk = followers[u'ids']
-	if (chunk and len(chunk) > 0):
+	if (chunk and len(chunk) > 0 and next_cursor not 0):
 		for item in chunk:
 			op_list.append(item)
-		return get_chunkList(api_object, screen_name, next_cursor, op_list)
+		return get_chunkList(screen_name=screen_name, api_object=api_object, cursor=next_cursor, op_list=op_list)
 	elif (next_cursor == 0):
 		return op_list
-
-def auth():
-	bearer_token = oauth2_dance(parameters['consumer_key'],parameters['consumer_secret'])
-	api = Twitter(auth=OAuth2(bearer_token=bearer_token), retry=True)
-	return api
 
 
 
 if __name__=='__main__':
+	print "Logged into twitter..."
 	try:
-		print "Logging into twitter..."
-		api = auth()
-		print "Authenticated."
-	except:
-		print "Unable to authenticate with twitter. Check access tokens."
-		raise SystemExit
-
 		user = raw_input('Enter a username to get followers for: ')
 
 		cursor = -1
 
 		get_chunkCSV(api, user, cursor)
+	except:
+		print "Unable to authenticate with twitter. Check access tokens."
+		raise SystemExit
+
